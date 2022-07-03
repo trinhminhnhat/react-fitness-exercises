70ea90e477daf6a45ac4cfb16589eb727c0c1745
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-07-03 08:39:27 +0700

    + update search exercises component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import { Box } from '@mui/material';
+import { useState } from 'react';
 import Exercises from '../components/Exercises';
 import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
+    const [exercises, setExercises] = useState([]);
+    const [bodyPart, setBodyPart] = useState('all');
+
     return (
         <Box>
             <HeroBanner />
-            <SearchExercises />
+            <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
             <Exercises />
         </Box>
     );
---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const exerciseOptions = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_EXERCISE_RAPID_API_KEY,
+        'X-RapidAPI-Host': process.env.REACT_APP_EXERCISE_RAPID_API_HOST,
+    },
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+};
